97eaf858c0cb0d2d2a588af507924f5708c54ab6
Karthi1945 <167897358+Karthi1945@users.noreply.github.com>
2024-12-30 15:37:57 +0530

    only radio button remaining

--- scritp.js
+++ scritp.js
@@ -5,7 +5,7 @@ const trans = document.querySelector("#trans");
 const form = document.querySelector("#form");
 const description = document.querySelector("#desc");
 const amount = document.querySelector("#amount");
-const category = document.querySelector("#category")
+const category = document.querySelector("#category");
 
 const localStorageTrans = JSON.parse(localStorage.getItem("trans"));
 let transactions = localStorage.getItem("trans") !== null ? localStorageTrans : [];
@@ -16,13 +16,14 @@ function loadTransactionDetails(transaction) {
   item.classList.add(transaction.category == 'Expenses' ? "exp" : "inc");
   item.innerHTML = `
     ${transaction.description}
-    <span>${sign} ${Math.abs(transaction.amount)}</span>
+    <span>${sign} ${(transaction.amount)}</span>
+    
     <span>${transaction.category}</span>
     <button class="btn-del" onclick="removeTrans(${transaction.id})">x</button>
-    <button class="btn-edt" onclick="editTrans(${transaction.id})">Edit</button>
+   
   `;
   trans.appendChild(item);
-  //console.log(transaction);
+ 
 }
 
 function removeTrans(id) {
@@ -36,50 +37,25 @@ function removeTrans(id) {
 }
 
 
-function editTrans(id) {
-    if (confirm("Are you sure you want to edit Transcation?")) {
 
-      const edit = transactions.find((transaction) => transaction.id == id)
-      
-      document.getElementById("amount").value = edit.amount;
-      document.getElementById("category").value = edit.category;
-      document.getElementById("desc").value = edit.date;
-      transactions = edit.filter(edit => edit.id !== id);
-      
-      config();
-      updateLocalStorage();
-    } else {
-      return;
-    }
+
+  function updateAmount() {
+    const amounts = transactions.map((transaction) => transaction.amount);
+   
+    
+    const income = transactions.filter((transaction) => transaction.category === "Income").map((transaction) => transaction.amount);
+    const income1 =income.reduce((acc, item) => (acc += Number(item)), 0);
+    inc_amt.innerHTML = `₹  ${income1}`;
+    
+    const category1 =  transactions.filter((transaction) => transaction.category === "Expenses").map((transaction) => transaction.amount);
+    const expense = category1.reduce((acc, item) => (acc += Number(item)), 0);
+    exp_amt.innerHTML = `₹  ${Math.abs(expense)}`;
+
+    const total = expense - income1;
+    balance.innerHTML = `₹  ${Math.abs(total)}`;
+  
   }
 
-function updateAmount() {
-  const amounts = transactions.map((transaction) => transaction.amount);
- const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-  balance.innerHTML = `₹  ${total}`;
-  
-  const income = amounts
-    .filter((item) => item > 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-  inc_amt.innerHTML = `₹  ${income}`;
-
-  const category = transactions.map((transaction) => transaction.category);
-  if(category == 'Expenses'){
-    const expense= amounts.reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-    console.log(category);
-    console.log(expense);
-  exp_amt.innerHTML = `₹  ${Math.abs(expense)}`;}
-
-  /*const expense = amounts
-    .filter((item) => item < 0)
-    .reduce((acc, item) => (acc += item), 0)
-    .toFixed(2);
-    console.log(expense)
-  exp_amt.innerHTML = `₹  ${Math.abs(expense)}`;*/
-
-}
 function config() {
   trans.innerHTML = "";
   transactions.forEach(loadTransactionDetails);
@@ -94,8 +70,10 @@ function addTransaction(e) {
     const transaction = {
       id: uniqueId(),
       description: description.value,
-      amount: +amount.value,
+      amount: amount.value,
       category : category.value,
+      
+      
     };
     transactions.push(transaction);
     loadTransactionDetails(transaction);
